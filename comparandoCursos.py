da89f17f545fa60137315147a791060ee3448383
Maico-Zurbriggen <zurbriggenmaico8@gmail.com>
2024-07-05 23:25:10 -0300

    Modifique el ejercicio practico 1 agregando la funcion round para redondear de forma mas eficiente

--- comparandoCursos.py
+++ comparandoCursos.py
@@ -10,15 +10,15 @@ crudo_otros_cursos_promedio = 5
 
 #Calculo de que porcentaje dura el video de dalto respecto de los otros videos
 diferencia_min = este_curso / otros_cursos_min * 100
-diferencia_max = este_curso * 1000// otros_cursos_max / 10 #Esto es para recortar un poco los decimales
+diferencia_max = round((este_curso / otros_cursos_max * 100), 2) #Esto es para recortar un poco los decimales
 diferencia_promedio = este_curso / otros_cursos_promedio * 100
 
 #Calculo de que porcentaje de video se elimina
-porcentaje_eliminado_este_curso = 100 - este_curso * 1000 // crudo_este_curso / 10
+porcentaje_eliminado_este_curso = round((100 - este_curso / crudo_este_curso * 100), 2)
 porcentaje_eliminado_otros_cursos_promedio = 100 - otros_cursos_promedio / crudo_otros_cursos_promedio * 100
 
 #Cuanto equivalen 10 horas de este curso en los otros cursos
-equivalencia_promedio = 10 / este_curso * 100 * otros_cursos_promedio // 100
+equivalencia_promedio = round((10 / este_curso * otros_cursos_promedio), 2)
 
 print(f"El curso de dalto dura un {diferencia_min}% del curso más rápido, un {diferencia_max}% del más lento y un {diferencia_promedio}% de los de velocidad media.")
 print(f"Este curso elimina el {porcentaje_eliminado_este_curso}% de los videos y el promedio de los demas cursos elimina el {porcentaje_eliminado_otros_cursos_promedio}%")
